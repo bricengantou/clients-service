ce4fd2bec3e346b6e5174996cf2bc75fb1efcde5
Brice Ngantou <81494218+briceigorngantou@users.noreply.github.com>
2024-03-27 13:29:08 +0100

    little update

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,6 +7,6 @@ export class AppController {
 
   @Get()
   getDocuments(): Promise<any> {
-    return this.appService.getDocument('clients');
+    return this.appService.getDocument('users');
   }
 }
